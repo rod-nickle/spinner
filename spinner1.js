3bccbdbff84b929fcc811f5ab727de53233eb646
rod-nickle <rod@bitwise-it.com>
2024-10-23 09:20:57 -0600

    Added spinner2.js

--- spinner1.js
+++ spinner1.js
@@ -20,23 +20,19 @@ setTimeout(() => {
 }, 900);
 
 setTimeout(() => {
-  process.stdout.write('\r|   ');
+  process.stdout.write('\r/   ');
 }, 1100);
 
 setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1300);
-
-setTimeout(() => {
   process.stdout.write('\r-   ');
-}, 1500);
+}, 1300);
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
   process.stdout.write('\r\\   ');
-}, 1700);
+}, 1500);
 
 setTimeout(() => {
   process.stdout.write('\r|   \n');
-}, 1900);
+}, 1700);
 
---
+++ spinner2.js
@@ -0,0 +1,54 @@
+/*
+| 0 4 8
+/ 1 5
+- 2 6
+\ 3 7
+
+*/
+// Eight is the number of positions we need to show a line for the spinner for one revolution.
+// NOTE: We will display the vertical line "|" an extra time at the end.
+// There are 4 different lines that we need to show.
+// Therefore we can determine the line to show based on the remainder of dividing the index by 4.
+const numberOfPositionsInOneRevolution = 8;
+for (let i = 0; i <= numberOfPositionsInOneRevolution; i++) {
+  let delay = 100;   // Start the delay at 100ms.
+  let char = "";
+  let remainder = 0;
+  
+  // Increment the by 200ms for each itertion of the loop.
+  delay += (delay + (200 * i));
+
+  // Determine the remainder of dividing the index by 4.
+  if (i !== 0) {
+    remainder = i % 4;
+  }
+
+  // The line that is displayed is dependent on the remainder.
+  switch (remainder) {
+  case 0:
+    char = '\r|   ';
+    break;
+  case 1:
+    char = '\r/   ';
+    break;
+  case 2:
+    char = '\r-   ';
+    break;
+  case 3:
+    char = '\r\\   ';
+    break;
+  }
+
+  // If we displaying the last line, add a new line character to move the cursor to a new line.
+  if (i === numberOfPositionsInOneRevolution) {
+    char += '\n';
+  }
+
+  // Print out the line after the appropriate delay.
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+
+}
+
+
